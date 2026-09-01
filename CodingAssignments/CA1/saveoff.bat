ca80f45df062154a02c4c648a58b3f47e162063f
Mark Synowiec <mark@enilion.com>
2026-09-01 17:40:20 -0500

    Update problem 2 to remove n=40 and use first 500 for training.

---
+++ CodingAssignments/CA1/saveoff.bat
@@ -0,0 +1,7 @@
+git pull
+git checkout ca1_mark
+copy CS598_PSL_CA_1_Mark.ipynb ..\..\..\CS598_PSL_CA_1_Mark.ipynb
+git checkout liam-ca1
+copy CS598_PSL_CA_1_Liam.ipynb ..\..\..\CS598_PSL_CA_1_Liam.ipynb
+git checkout ca1_Ryan
+copy CS598_PSL_CA_1_Ryan.ipynb ..\..\..\CS598_PSL_CA_1_Ryan.ipynb
